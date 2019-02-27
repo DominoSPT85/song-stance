96157a6e5dfc5ea4ceee46eb3618ec99385526ae
Shandra <shandradev85@gmail.com>
2019-02-26 22:58:03 -0700

    added edit and delete

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,9 +7,11 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+
   def new
     @song = Song.new
   end
+
 
   def create
     @song = Song.new(songs_params)
@@ -21,10 +23,33 @@ class SongsController < ApplicationController
     end
   end
 
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.update(song_params)
+      redirect_to songs_path
+    else
+      render :edit
+    end
+  end
+
+  
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
+
   private
 
     def songs_params
       params.require(:song).permit(:title, :artist, :review)
     end
-    
+
 end
