f6c7441bfbeb1cd58870db7c18b3798150d85201
Shandra <shandradev85@gmail.com>
2019-03-01 17:31:34 -0700

    building reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,62 @@
 class ReviewsController < ApplicationController
-  def index
-  end
+  before_action :set_song
 
-  def new
+  before_action :find_review, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @reviews = @song.reviews
   end
 
   def show
+
   end
+
+  def new
+    @reviews = @song.reviews.new
+    # render partial: 'reviews/form'
+  end
+
+  def create
+    @review = @song.reviews.new(review_params)
+
+    if @review.save
+      redirect_to [@song, @review]
+    else
+      render :new
+    end
+  end
+
+  def edit
+    #render partial: 'reviews/form'
+
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to [@song, @review]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to sons_reviews_path
+
+
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:body, :author)
+    end
+
+    def set_song
+      @song = Song.find(params[:id])
+    end
+
+    def find_review
+      @review = Review.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/show'
+
   root 'static_pages#home'
 
   get '/about', to: 'static_pages#about'
 
-  resources :songs
+  resources :songs do
+    resources :reviews
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
